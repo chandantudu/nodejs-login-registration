535a721b5d25e8c5c1fe73809dc96d2cedc41d7c
DavidLuise <108882390+DavidLuise@users.noreply.github.com>
2022-07-26 00:22:46 -0400

    001_work_modified by David

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -4,7 +4,7 @@ const dbConnection = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'nodejs_login'
+    database: 'auth_db'
 });
 
 module.exports = dbConnection.promise();
